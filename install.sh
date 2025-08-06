0ab8543ee7ad3f8f4bf820c8ee222738f1283682
Rust <drmayankthakre25@gmail.com>
2025-08-06 21:14:40 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/monkdluffy25/barny_device_xiaomi_raphael.git device/xiaomi/raphael
+git clone https://github.com/SOVIET-ANDROID/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/SOVIET-ANDROID/kernel_xiaomi_raphael.git kernel/xiaomi/raphael
+git clone https://github.com/SOVIET-ANDROID/vendor_xiaomi_raphael.git vendor/xiaomi/raphael
+git clone https://github.com/SOVIET-ANDROID/device_xiaomi_raphael-kernel.git device/xiaomi/raphael-kernel
