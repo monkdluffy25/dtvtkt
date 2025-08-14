38733332ee8d9f584cf73507fba163819c471262
Rust <drmayankthakre25@gmail.com>
2025-08-15 01:19:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/monkdluffy25/barny_device_xiaomi_raphael.git device/xiaomi/raphael
-git clone https://github.com/LineageOS/android_hardware_xiaomi - b lineage-23.0 hardware/xiaomi
-git clone https://github.com/SOVIET-ANDROID/kernel_xiaomi_raphael.git kernel/xiaomi/raphael
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-23.0 hardware/xiaomi
+git clone https://github.com/SOVIET-ANDROID/kernel_xiaomi_raphael.git -b 16.0-susfs kernel/xiaomi/raphael
 git clone https://github.com/SOVIET-ANDROID/vendor_xiaomi_raphael.git vendor/xiaomi/raphael
 git clone https://github.com/SOVIET-ANDROID/device_xiaomi_raphael-kernel.git device/xiaomi/raphael-kernel
